103af7540ec7c1c1b5d5e29780c282137a9dced1
NMZ0429 <gen0429@icloud.com>
2021-05-10 20:23:24 +0900

    update ui

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 
 # layout setting
-st.set_page_config(page_title="namazu-tabular",page_icon=None,layout='wide',initial_sidebar_state="collapsed")
+st.set_page_config(page_title="Made by Namazur",page_icon=None,layout='wide',initial_sidebar_state="collapsed")
 hide_streamlit_style = """
                         <style>
                         #MainMenu {visibility: hidden;}
@@ -29,14 +29,19 @@ from file_downloader import get_binary_file_downloader_html
 st.markdown("<h1 style='text-align: center;'>Sounds concatenator</h1>", unsafe_allow_html=True)
 st.empty()
 
-st.markdown("## Import first audio file")
-first = st.file_uploader("first sound", type=['mp3','m4a'], key='a', accept_multiple_files=False)
-st.markdown("## Import second audio file")
-second = st.file_uploader("second sound", type=['mp3','m4a'], key='b', accept_multiple_files=False)
+st.markdown("## 1. Import first audio file")
+first = st.file_uploader("", type=['mp3','m4a'], key='a', accept_multiple_files=False)
+st.audio(first)
+st.empty()
+st.markdown("## 2. Import second audio file")
+second = st.file_uploader("", type=['mp3','m4a'], key='b', accept_multiple_files=False)
+st.audio(second)
+
+confirmed = st.button('Start')
 
 from pydub import AudioSegment
 
-if (first and second):
+if (first and second) and confirmed: 
     # 音声ファイルの読み込み
     sound1 = AudioSegment.from_file(first)
     sound2 = AudioSegment.from_file(second)
@@ -46,6 +51,10 @@ if (first and second):
 
     # 保存
     sound.export("output.mp3", format="mp3")
+
+    # ダウンロードリンク生成
     st.markdown("Your audio file is ready for download")         
     st.markdown(get_binary_file_downloader_html("output.mp3", 'the sound file'), unsafe_allow_html=True)
+    
+    # ファイル消去
     os.remove("output.mp3")
