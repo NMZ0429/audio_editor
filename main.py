d09540351e3ba59302f9672db1bf765c37e6a26a
NMZ0429 <gen0429@icloud.com>
2021-05-10 22:48:40 +0900

    accept multiple files

--- main.py
+++ main.py
@@ -29,28 +29,25 @@ from file_downloader import get_binary_file_downloader_html
 st.markdown("<h1 style='text-align: center;'>Sounds concatenator</h1>", unsafe_allow_html=True)
 st.empty()
 
-st.markdown("## 1. Import first audio file")
-first = st.file_uploader("", type=['mp3','m4a'], key='a', accept_multiple_files=False)
-st.audio(first)
+st.markdown("## Import first audio files")
+st.markdown("### Upload files in order such that the files are concatenated in that way. If you upload two files A first then B, resultant audio file plays A then B.\n\n You can drag and drop file one by one or multiple files at once.")
+audio_files = st.file_uploader("", type=['mp3','m4a'], key='a', accept_multiple_files=True)
 st.empty()
-st.markdown("## 2. Import second audio file")
-second = st.file_uploader("", type=['mp3','m4a'], key='b', accept_multiple_files=False)
-st.audio(second)
 
+num_files = len(audio_files)
 confirmed = st.button('Start')
 
 from pydub import AudioSegment
 
-if (first and second) and confirmed: 
+if (num_files > 0) and confirmed: 
     # 音声ファイルの読み込み
-    sound1 = AudioSegment.from_file(first)
-    sound2 = AudioSegment.from_file(second)
-
-    # 連結
-    sound = sound1 + sound2
+    combined_sound = AudioSegment.empty()
+    for i in audio_files:
+        sound_i = AudioSegment.from_file(i)
+        combined_sound += sound_i
 
     # 保存
-    sound.export("output.mp3", format="mp3")
+    combined_sound.export("output.mp3", format="mp3")
 
     # ダウンロードリンク生成
     st.markdown("Your audio file is ready for download")         
